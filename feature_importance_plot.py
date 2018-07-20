9061be365aa11b74814dbb829e4cc613170fd823
juancq <juancq@gmail.com>
2018-07-20 16:42:21 +1000

    Change boxplot colors and add legends to subplots

--- feature_importance_plot.py
+++ feature_importance_plot.py
@@ -24,10 +24,33 @@ def aggregate_impor(impor):
     indices = sorted(indices, key=lambda i: medians[i], reverse=True)
     return np.array(new_impor).T[indices], indices
 
+
+def make_legend(colors):
+    """Make a legend for the acc, gyro, and heart sensor data"""
+
+    acc_patch = mpatches.Patch(label='Acc', 
+                            facecolor=colors[0], edgecolor='black')
+    gyro_patch = mpatches.Patch(label='Gyro', 
+                            facecolor=colors[1], edgecolor='black')
+    heart_patch = mpatches.Patch(label='Heart', 
+                            facecolor=colors[2], edgecolor='black',
+                            hatch=r'\\')
+
+    plt.rcParams["legend.fontsize"] = 11.5
+
+    l = plt.legend(handles=[heart_patch, acc_patch, gyro_patch], 
+                    loc=1, frameon=False,
+                    ncol=3, borderaxespad=0.2, 
+                    columnspacing=0.7, handletextpad=0.3, 
+                    fancybox=True, framealpha=0.3,
+                    bbox_to_anchor=(0., 1.07, 1., .102))
+    l.get_frame().set_facecolor('white')
+    return l
+
+
 def main():
-    '''
-    Creates feature importances plot divided by condition.
-    '''
+    ''' Creates feature importances plot divided by condition.  '''
+
     parser = argparse.ArgumentParser("plot accelerometer data")
     parser.add_argument("-mo", type=str, help="movie yaml")
     parser.add_argument("-mu", type=str, help="music yaml")
@@ -49,9 +72,12 @@ def main():
     music_walk = yaml.load(open(args.mw))
     data = [movie['rf'], music['rf'], music_walk['rf']]
 
-    colors=['darkseagreen', 'plum', 'sandybrown']
+    # colors for different sensors in boxplots
+    colors=['lightblue', 'white', 'thistle']
+    # titles for subplots
     titles = ['Movie', 'Music', 'Music while walking']
 
+    # labels for features 
     labels_unsorted = [s.strip() for s in open('feature_list').readlines()]
 
     plt.figure(figsize=(9,9))
@@ -62,12 +88,9 @@ def main():
             share = plt.subplot(3, 1, i+1, sharex=share)
         else:
             share = plt.subplot(3, 1, i+1)
-            acc_patch = mpatches.Patch(color=colors[0], label='Acc')
-            gyro_patch = mpatches.Patch(color=colors[1], label='Gyro')
-            heart_patch = mpatches.Patch(color=colors[2], label='Heart')
-            plt.rcParams["legend.fontsize"] = 10
-            plt.legend(handles=[heart_patch, acc_patch, gyro_patch], bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-            ncol=3, borderaxespad=0.)
+
+        l1 = make_legend(colors)
+        plt.gca().add_artist(l1)
 
         feature_impor = group
 
@@ -75,7 +98,14 @@ def main():
         labels = [labels_unsorted[s] for s in indices]
 
         limit = 30
-        bp = plt.boxplot(feature_impor_all[:limit].T, patch_artist=True, labels=labels[:limit])#, widths=0.1)#, showmeans=True)
+        bp = plt.boxplot(feature_impor_all[:limit].T, patch_artist=True, 
+                    labels=labels[:limit], sym='+')
+
+        # change color of median line
+        for median in bp['medians']:
+            median.set(color='red', linewidth=1.8)
+
+        # set color of boxplot based on sensor type
         for j, box in enumerate(bp['boxes']):
             if 'acc_' in labels[j] or 'mag' in labels[j]:
                 box.set(facecolor=colors[0])
@@ -83,6 +113,7 @@ def main():
                 box.set(facecolor=colors[1])
             elif 'heart' in labels[j]:
                 box.set(facecolor=colors[2])
+                box.set(hatch = r'\\')
 
         short_labels = [re.sub(r'^(acc_|gyro_)', '', l) for l in labels[:limit]]
         plt.xticks(range(1,limit+1), short_labels, fontsize=9, rotation=70)
@@ -93,14 +124,12 @@ def main():
         share.tick_params(labelright=True)
         plt.title(title, fontsize=12)
 
-        #loc = plticker.MultipleLocator(base=0.1) 
-        #share.yaxis.set_major_locator(loc)
 
-    plt.suptitle('Feature Importances per Condition', fontsize=12)
+    plt.suptitle('Feature Importances per Condition', fontsize=13)
 
     plt.subplots_adjust(wspace=1.)
     plt.tight_layout()
-    plt.subplots_adjust(top=0.93)
+    plt.subplots_adjust(top=0.92)
     plt.savefig(output_file + '.png', bbox_inches='tight')
 
 
